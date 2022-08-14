61aabf138e9d83bd2dd728051efd6490cc7979c2
curlybrace_laptop <curlybracemaster@gmail.com>
2022-08-14 17:07:49 -0400

    added props to ExpenseItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './styles/App.css';
 import ExpenseItem from './components/ExpenseItem';
 
+const expenses = [
+	{
+		id: 'e1',
+		title: 'toilet paper',
+		amount: 94.12,
+		date: new Date(2020, 7, 14)
+	},
+	{
+		id: 'e2',
+		title: 'new tv',
+		amount: 799.49,
+		date: new Date(2021, 2, 12)
+	},
+	{
+		id: 'e3',
+		title: 'car insurance',
+		amount: 294.67,
+		date: new Date(2021, 2, 20)
+	},
+	{
+		id: 'e4',
+		title: 'new desk',
+		amount: 450,
+		date: new Date(2021, 5, 12)
+	},
 
-function App(props) {
+];
+
+function App() {
 	return (
 		<div className="App">
-			<ExpenseItem/>
+			<ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}/>
+			<ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}/>
+			<ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}/>
+			<ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}/>
 		</div>
 	);
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,14 @@
 import '../styles/ExpenseItem.css';
 
-const date = Date();
-const expenseTitle = 'car insurance';
-const expenseAmount = 294.99;
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 	return (
 		<div className="expense-item">
-			<div>{date}</div>
+			<div>{props.date.toISOString()}</div>
 			<div className="expense-item__description">
-				<h2>{expenseTitle}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">
-					{expenseAmount}
+					{props.amount}
 				</div>
 			</div>
 		</div>
