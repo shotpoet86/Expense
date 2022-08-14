24e7affdef033ed7fa05864d1a59283513453309
curlybrace <curlybracemaster@gmail.com>
2022-08-14 16:07:43 -0400

    init commit

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,21 @@
+import '../styles/ExpenseItem.css';
+
+const date = Date();
+const expenseTitle = 'car insurance';
+const expenseAmount = 294.67;
+
+function ExpenseItem() {
+	return (
+		<div className="expense-item">
+			<div>{date}</div>
+			<div className="expense-item__description">
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price">
+					{expenseAmount}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
