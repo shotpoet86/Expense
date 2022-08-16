6f9722114ce12489c65e0464f58cbf924b12c34e
curlybrace_laptop <curlybracemaster@gmail.com>
2022-08-15 23:31:16 -0400

    seperated funcitonality into multiple

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -4,7 +4,7 @@ function ExpenseDate(props) {
 	const month = props.date.toLocaleString('en-US', {month: 'long'});
 	const day = props.date.toLocaleString('en-US', {day: '2-digit'});
 	const year = props.date.getFullYear();
-
+	/*test comment*/
 	return (
 		<div className="expense-date">
 			<div className="expense-date__month">{month}</div>
